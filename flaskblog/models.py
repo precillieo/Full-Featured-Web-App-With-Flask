ec3d734b8256b7b42379afdb1a031671a68ac40c
Precious Kolawole <kolawoleprecious99@gmail.com>
2021-01-09 13:39:57 +0100

    Update models.py

--- flaskblog/models.py
+++ flaskblog/models.py
@@ -33,6 +33,7 @@ class User(db.Model, UserMixin):
 	def __repr__(self):
 		return f"User('{self.username}','{self.email}', '{self.image}')"
 
+
 class Post(db.Model):
 	id= db.Column(db.Integer, primary_key= True)
 	title= db.Column(db.String(100), nullable= False)
